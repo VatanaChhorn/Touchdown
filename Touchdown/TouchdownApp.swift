b4faeb722f65fe1d1383b5ae6e16715c32633e09
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2021-01-08 11:34:16 +0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Vatana Chhorn on 08/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
